531431c697be37e890de11082555ba256d13bea9
afreeman123 <freeman.aidan@gmail.com>
2016-01-24 15:08:07 +0000

    Added comments to start.java

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,5 +1,7 @@
+//everything begins with a class
 class apples {
   public static void main(String args[]){
+    //println() built in method to print a line to db, cmd etc.
     System.out.println("Hello World!");
   }
 }
