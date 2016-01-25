906ee90f6e880926befe23b0cf8a6433886d5109
afreeman123 <freeman.aidan@gmail.com>
2016-01-25 23:04:55 +0000

    added double var type

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -5,6 +5,9 @@ class apples {
     double tuna;
     tuna = 5.28;
 
+    System.out.print("I want ");
     System.out.print(tuna);
+    System.out.println(" movies");
+    System.out.println("apples");
   }
 }
