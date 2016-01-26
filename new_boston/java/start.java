19460a283602fe3014bdb994cd5ac84aff250846
afreeman123 <freeman.aidan@gmail.com>
2016-01-26 18:36:11 +0000

    Built a basic calculator

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -7,7 +7,15 @@ class apples {
     //System.in means system input
     Scanner aidan =  new Scanner(System.in);
 
-    //.nextLine() allows a pause for input
-    System.out.println(aidan.nextLine());
+    double num1, num2, answer;
+
+    System.out.println("Enter first number: ");
+    num1 = aidan.nextDouble();
+
+    System.out.println("Enter second number: ");
+    num2 = aidan.nextDouble();
+
+    answer = num1 + num2;
+    System.out.println("The answer is " + answer);
   }
 }
