0f51f4bc468b12eae27e368dc0f111501e295d3a
afreeman123 <freeman.aidan@gmail.com>
2016-01-26 23:20:11 +0000

    Math Operators

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 //everything begins with a class
 class apples {
   public static void main(String args[]){
+
     //Scanner allows user input
     //System.in means system input
     Scanner aidan =  new Scanner(System.in);
 
-    double num1, num2, answer;
-
-    System.out.println("Enter first number: ");
-    num1 = aidan.nextDouble();
-
-    System.out.println("Enter second number: ");
-    num2 = aidan.nextDouble();
-
-    answer = num1 + num2;
-    System.out.println("The answer is " + answer);
+    int girls, boys, people;
+    girls = 10;
+    boys = 3;
+    //modulus
+    people =  girls % boys;
+    System.out.println(people);
   }
 }
