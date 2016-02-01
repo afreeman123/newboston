259b3439144915e3531e48d139409679384ff949
afreeman123 <freeman.aidan@gmail.com>
2016-02-01 23:14:05 +0000

    Added parameters to tuna class

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 //everything begins with a class
 class apples {
   public static void main(String args[]){
-    tuna aidan = new tuna();
 
-    aidan.simpleMessage();
+      Scanner input = new Scanner(System.in);
+      tuna tunaObject = new tuna();
+
+      System.out.println("Enter your name here: ");
+      String name = input.nextLine();
+
+      tunaObject.simpleMessage(name);
+
   }
 }
--- new_boston/java/tuna.java
+++ new_boston/java/tuna.java
@@ -1,6 +1,7 @@
 public class tuna {
   //void means its gonna do something but won't return any data
-  public void simpleMessage() {
-    System.out.println("This is the tuna class");
+  public void simpleMessage(String name) {
+    System.out.println("Hello " + name);
   }
+
 }
