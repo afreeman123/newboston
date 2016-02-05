3840f873755ed5b57d0c1129b4b0c93ee84b2415
afreeman123 <freeman.aidan@gmail.com>
2016-02-05 18:03:09 +0000

    Create a post-check while loop

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,16 +1,13 @@
 class apples {
   public static void main(String args[]){
-    //Compund interest formula: A=P(1+R)^n where n = no. of years
-    //P = current amount, R =  interest
-    double amount;
-    double principal = 10000;
-    double rate = 0.01;
 
-    for(int day=1;day<=20;day++) {
-      //Math.pow times the 2 parameters together
-      amount = principal * Math.pow(1 + rate, day);
-      System.out.println(day + "  " + amount);
-    }
+    int counter = 0; //if counter = 15, output: 15
+
+    //post-check while loop
+    do {
+      System.out.println(counter);
+      counter++;
+    }while(counter <= 10);
 
   }
 }
