0e0829bdfed0a76fc37a4ca43e23e19cd1e9561d
afreeman123 <freeman.aidan@gmail.com>
2016-02-07 13:23:23 +0000

    Create a random number generator

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,26 +1,17 @@
+//
+import java.util.Random;
+
 class apples {
   public static void main(String args[]){
-    //Math class has various calculating methods within it
+    //Program to simulate a dice
 
-    //Math.abs() finds the absoulute value of the parameter given
-    System.out.println(Math.abs(-26.7));
+    Random dice = new Random();
+    int number;
 
-    //Math.ceil() rounds up
-    System.out.println(Math.ceil(7.4));
-
-    //Math.floor() rounds down
-    System.out.println(Math.floor(5.6));
-
-    //Math.max() takes multiple parameters and finds the max of the those numbers
-    System.out.println(Math.max(8.4,5.2));
-
-    //Math.min() takes multiple parameters and finds the min of the those numbers
-    System.out.println(Math.min(8.4,5.2));
-
-    //Math.pow(x,y) takes two parameters and calculates x^y
-    System.out.println(Math.pow(5,3));
-
-    //Math.sqrt() find the square root of the value given
-    System.out.println(Math.sqrt(9));
+    for(int counter=1;counter<=10;counter++) {
+      //takes a random value between 0 and 5
+      number = dice.nextInt(6);
+      System.out.println(number);
+    }
   }
 }
