78334a2005ee21effe5ac2494d14238c273a873b
afreeman123 <freeman.aidan@gmail.com>
2016-02-07 01:08:23 +0000

    Use the Math class to do some calculations

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,13 +1,26 @@
 class apples {
   public static void main(String args[]){
+    //Math class has various calculating methods within it
 
-    int counter = 0; //if counter = 15, output: 15
+    //Math.abs() finds the absoulute value of the parameter given
+    System.out.println(Math.abs(-26.7));
 
-    //post-check while loop
-    do {
-      System.out.println(counter);
-      counter++;
-    }while(counter <= 10);
+    //Math.ceil() rounds up
+    System.out.println(Math.ceil(7.4));
 
+    //Math.floor() rounds down
+    System.out.println(Math.floor(5.6));
+
+    //Math.max() takes multiple parameters and finds the max of the those numbers
+    System.out.println(Math.max(8.4,5.2));
+
+    //Math.min() takes multiple parameters and finds the min of the those numbers
+    System.out.println(Math.min(8.4,5.2));
+
+    //Math.pow(x,y) takes two parameters and calculates x^y
+    System.out.println(Math.pow(5,3));
+
+    //Math.sqrt() find the square root of the value given
+    System.out.println(Math.sqrt(9));
   }
 }
