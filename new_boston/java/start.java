614dc0cdf9bc859ace3a306bb36e7664789b106a
afreeman123 <freeman.aidan@gmail.com>
2016-02-20 18:53:20 +0000

    Use an elipse

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,29 +1,19 @@
 class apples {
   public static void main(String args[]){
-    //multidimensional array
-    int firstarray[][] = {{8,9,10,11},{12,13,14,15}};
-    //can have different lengths
-    int secondarray[][] = {{30,31,32,33},{43},{4,5,6}};
-
-    System.out.println("This is the first array");
-    display(firstarray);
-
-    System.out.println();
-    
-    System.out.println("This is the second array");
-    display(secondarray);
+    System.out.println(average(45,46,32,89,67));
   }
 
-  public static void display(int x[][]) {
-    //loop for rows
-    for(int row=0;row<x.length;row++) {
-      //loop for columns
-      for(int column=0;column<x[row].length;column++) {
-        //print row
-        System.out.print(x[row][column] + "\t");
-      }
-      //create new line after printing row
-      System.out.println();
+  //... = an elipse which means you dont know how many of that type you will
+  //be using as parameters
+  public static int average(int...numbers) {
+
+    int total = 0;
+    for (int x:numbers) {
+      total += x;
     }
+
+    return total/numbers.length;
+
   }
+
 }
