cf6ede4d796b17529795b1d7ce0c8bc96ec82b90
afreeman123 <freeman.aidan@gmail.com>
2016-03-02 21:17:15 +0000

    Add to the time class and create a toString method

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,11 +1,7 @@
 class apples {
   public static void main(String args[]){
-      tuna tunaObject = new tuna();
 
-      System.out.println(tunaObject.toMilitary());
-      tunaObject.setTime(13, 27, 6);
+    potpie potObject = new potpie(4,5,6);
 
-      System.out.println(tunaObject.toMilitary());
-      System.out.println(tunaObject.toString());
   }
 }
