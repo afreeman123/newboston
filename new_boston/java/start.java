99662d4607fdfc2e20e7e73af76e5ee0b50fe475
afreeman123 <freeman.aidan@gmail.com>
2016-03-09 18:55:50 +0000

    Used toString method and composition

--- new_boston/java/start.java
+++ new_boston/java/start.java
@@ -1,7 +1,10 @@
 class apples {
   public static void main(String args[]){
 
-    potpie potObject = new potpie(4,5,6);
+    potpie potObject = new potpie(2,11,1995);
+    tuna tunaObject = new tuna("Aidan", potObject);
+
+    System.out.println(tunaObject);
 
   }
 }
--- new_boston/java/tuna.java
+++ new_boston/java/tuna.java
@@ -1,24 +1,16 @@
 public class tuna {
-  private int hour;
-  private int minute;
-  private int second;
+  private String name;
+  //can have type of custom classes
+  private potpie birthday;
 
-  public void setTime(int h, int m, int s) {
-    //if it meets the condition before ? the do the thing before :
-    //else do thing after :
-    hour = ((h>=0 && h<24) ? h:0);
-    minute = ((m>=0 && m<60) ? m:0);
-    second = ((s>=0 && s<60) ? s:0);
-  }
-
-  //military time method
-  public String toMilitary() {
-    //"%02d:%02d:%02d" means return the string calling the method in the format
-    //2dp:2dp:2dp
-    return String.format("%02d:%02d:%02d", hour, minute, second);
+  public tuna(String theName, potpie theDate) {
+    name = theName;
+    birthday = theDate;
   }
 
   public String toString() {
-    return String.format("%d:%02d:%02d %s", ((hour==0||hour==12) ? 12:hour%12) , minute, second, (hour < 12 ? "AM":"PM"));
+    return String.format("My name is %s, my birthday is %s", name, birthday);
+    //birthday is an object of class potpie and so, looks for toString method
+    //in that class
   }
 }
